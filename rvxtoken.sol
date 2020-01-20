ed9f3573c174f3ced69cf38e7307e6e7a59c1b0b
flisko <ziga.flis@student.um.si>
2020-01-20 22:58:12 +0100

    Added modifier and functions
    Added operator modifier for faucet, added depositETH function, added default fallback (payable) function, added withdraw ETH from contract function.

--- rvxtoken.sol
+++ rvxtoken.sol
@@ -2,9 +2,6 @@ pragma solidity ^ 0.5 .11;
 
 
 library SafeMath {
-    /**
-     * @dev Returns the addition of two unsigned integers, reverting on overflow.
-     */
     function add(uint256 a, uint256 b) internal pure returns(uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
@@ -12,16 +9,10 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting on overflow (when the result is negative).
-     */
     function sub(uint256 a, uint256 b) internal pure returns(uint256) {
         return sub(a, b, "SafeMath: subtraction overflow");
     }
 
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on overflow (when the result is negative).
-     */
     function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns(uint256) {
         require(b <= a, errorMessage);
         uint256 c = a - b;
@@ -29,13 +20,7 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the multiplication of two unsigned integers, reverting on overflow.
-     */
     function mul(uint256 a, uint256 b) internal pure returns(uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
         if (a == 0) {
             return 0;
         }
@@ -46,103 +31,62 @@ library SafeMath {
         return c;
     }
 
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts on division by zero. The result is rounded towards zero.
-     */
     function div(uint256 a, uint256 b) internal pure returns(uint256) {
         return div(a, b, "SafeMath: division by zero");
     }
 
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on division by zero. The result is rounded towards zero.
-     */
     function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns(uint256) {
         // Solidity only automatically asserts when dividing by 0
         require(b > 0, errorMessage);
         uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
 
         return c;
     }
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo), Reverts when dividing by zero.
-     */
     function mod(uint256 a, uint256 b) internal pure returns(uint256) {
         return mod(a, b, "SafeMath: modulo by zero");
     }
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo), Reverts with custom message when dividing by zero.
-     */
     function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns(uint256) {
         require(b != 0, errorMessage);
         return a % b;
     }
 }
 contract Context {
-    // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, with should be used via inheritance.
     constructor() internal {}
-    // solhint-disable-previous-line no-empty-blocks
 
     function _msgSender() internal view returns(address) {
         return msg.sender;
     }
 
     function _msgData() internal view returns(bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        this;
         return msg.data;
     }
 }
 
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable is Context {
     address payable public _owner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
     constructor() internal {
         _owner = msg.sender;
         emit OwnershipTransferred(address(0), _owner);
     }
 
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
     modifier onlyOwner() {
         require(isOwner(), "Ownable: caller is not the owner");
         _;
     }
 
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
     function isOwner() public view returns(bool) {
         return _msgSender() == _owner;
     }
 
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
     function transferOwnership(address payable newOwner) public onlyOwner {
         _transferOwnership(newOwner);
     }
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
     function _transferOwnership(address payable newOwner) internal {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
@@ -151,35 +95,21 @@ contract Ownable is Context {
 
 }
 
-/**
- * @title Roles
- * @dev Library for managing addresses assigned to a Role.
- */
 library Roles {
     struct Role {
         mapping(address => bool) bearer;
     }
 
-    /**
-     * @dev Give an account access to this role.
-     */
     function add(Role storage role, address account) internal {
         require(!has(role, account), "Roles: account already has role");
         role.bearer[account] = true;
     }
 
-    /**
-     * @dev Remove an account's access to this role.
-     */
     function remove(Role storage role, address account) internal {
         require(has(role, account), "Roles: account does not have role");
         role.bearer[account] = false;
     }
 
-    /**
-     * @dev Check if an account has this role.
-     * @return bool
-     */
     function has(Role storage role, address account) internal view returns(bool) {
         require(account != address(0), "Roles: account is the zero address");
         return role.bearer[account];
@@ -227,128 +157,77 @@ contract MinterRole is Context {
     }
 }
 
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see {ERC20Detailed}.
- */
 interface IERC20 {
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
+
     function totalSupply() external view returns(uint256);
 
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
     function balanceOf(address account) external view returns(uint256);
 
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `recipient`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
     function transfer(address recipient, uint256 amount) external returns(bool);
 
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
     function allowance(address owner, address spender) external view returns(uint256);
 
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
     function approve(address spender, uint256 amount) external returns(bool);
 
-    /**
-     * @dev Moves `amount` tokens from `sender` to `recipient` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
     function transferFrom(address sender, address recipient, uint256 amount) external returns(bool);
-
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-
-/**
- * @dev Extension of {ERC20} that adds a set of accounts with the {MinterRole},
- * which have permission to mint (create) new tokens as they see fit.
- *
- * At construction, the deployer of the contract is the only minter.
- */
-
-
 contract RVXToken is MinterRole, Ownable, IERC20 {
-    using SafeMath for uint;
+
+    using SafeMath
+    for uint;
     bool private faucetEnabled = true;
     uint256 private testTokenAmount = 10 ** 21;
     string private _name;
     string private _symbol;
     uint8 private _decimals;
+    address public operator;
     mapping(address => uint256) private _balances;
-
     mapping(address => mapping(address => uint256)) private _allowances;
-
     uint256 private _totalSupply;
+    modifier onlyPayloadSize(uint size) {
+        require(msg.data.length >= size + 4);
+        _;
+    }
+
 
     modifier isFaucetEnabled {
         require(faucetEnabled);
         _;
     }
 
-    /**
-     * @dev Fix for the ERC20 short address attack.
-     */
-
-    modifier onlyPayloadSize(uint size) {
-        require(msg.data.length >= size + 4);
+    modifier onlyOperator {
+        require(_msgSender() == operator || _msgSender() == _owner);
         _;
     }
 
-    /**
-     * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
-     * these values are immutable: they can only be set once during
-     * construction.
-     */
+    function setOperator(address _operator) external onlyOwner {
+        require(_operator != address(0));
+        operator = _operator;
+    }
+    //constructor
     constructor() public {
         _name = "RiveX Token";
         _symbol = "RVX";
         _decimals = 18;
         _totalSupply = 4000000000 ether;
         _balances[msg.sender] = 4000000000 ether;
+        operator = 0x823FbD6C41Ff917B78B88fEE561291Fd750DDfCd; //added operator address (faucet wallet)
+    }
+
+    function depositETH() public payable { //added deposit eth function
+        require(msg.value > 0);
+    }
+
+    function() external payable { //added default fallback function (if someone just deposits eth to contract, it can handle)
+        depositETH();
+    }
+
+    function withdraw() onlyOwner public { //owner can withdraw ether from contract
+
+        _owner.transfer(address(this).balance);
     }
 
     function toggleFaucet() onlyOwner public {
@@ -364,12 +243,12 @@ contract RVXToken is MinterRole, Ownable, IERC20 {
     }
 
 
-    function changeTestTokenAmount(uint256 amount) public onlyOwner returns(bool) {
+    function changeTestTokenAmount(uint256 amount) public onlyOperator returns(bool) {
         testTokenAmount = amount * 1000000000000000000;
         return true;
     }
 
-    function transferTestToken(address wallet) public isFaucetEnabled returns(bool) {
+    function transferTestToken(address wallet) public isFaucetEnabled onlyOperator returns(bool) {
         _transfer(_msgSender(), wallet, testTokenAmount);
         return true;
     }
@@ -378,154 +257,57 @@ contract RVXToken is MinterRole, Ownable, IERC20 {
         _mint(account, amount);
         return true;
     }
-    /**
-     * @dev Returns the name of the token.
-     */
+
     function name() public view returns(string memory) {
         return _name;
     }
 
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
     function symbol() public view returns(string memory) {
         return _symbol;
     }
 
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
     function decimals() public view returns(uint8) {
         return _decimals;
     }
 
-
-
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
     function totalSupply() public view returns(uint256) {
         return _totalSupply;
     }
 
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
     function balanceOf(address account) public view returns(uint256) {
         return _balances[account];
     }
 
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
     function transfer(address recipient, uint256 amount) public returns(bool) {
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-
-    /**
-     * @dev See {IERC20-allowance}.
-     */
     function allowance(address owner, address spender) public view returns(uint256) {
         return _allowances[owner][spender];
     }
 
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
     function approve(address spender, uint256 amount) public returns(bool) {
         _approve(_msgSender(), spender, amount);
         return true;
     }
 
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for `sender`'s tokens of at least
-     * `amount`.
-     */
     function transferFrom(address sender, address recipient, uint256 amount) public returns(bool) {
         _transfer(sender, recipient, amount);
         _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
         return true;
     }
 
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
     function increaseAllowance(address spender, uint256 addedValue) public returns(bool) {
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
         return true;
     }
 
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
     function decreaseAllowance(address spender, uint256 subtractedValue) public returns(bool) {
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
         return true;
     }
 
-    /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`.
-     *
-     * This is internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
@@ -535,15 +317,6 @@ contract RVXToken is MinterRole, Ownable, IERC20 {
         emit Transfer(sender, recipient, amount);
     }
 
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `to` cannot be the zero address.
-     */
     function _mint(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: mint to the zero address");
 
@@ -552,17 +325,6 @@ contract RVXToken is MinterRole, Ownable, IERC20 {
         emit Transfer(address(0), account, amount);
     }
 
-    /**
-     * @dev Destroys `amount` tokens from `account`, reducing the
-     * total supply.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `account` cannot be the zero address.
-     * - `account` must have at least `amount` tokens.
-     */
     function _burn(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: burn from the zero address");
 
@@ -571,19 +333,6 @@ contract RVXToken is MinterRole, Ownable, IERC20 {
         emit Transfer(account, address(0), amount);
     }
 
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-     *
-     * This is internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     */
     function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
@@ -592,12 +341,6 @@ contract RVXToken is MinterRole, Ownable, IERC20 {
         emit Approval(owner, spender, amount);
     }
 
-    /**
-     * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-     * from the caller's allowance.
-     *
-     * See {_burn} and {_approve}.
-     */
     function _burnFrom(address account, uint256 amount) internal {
         _burn(account, amount);
         _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "ERC20: burn amount exceeds allowance"));
