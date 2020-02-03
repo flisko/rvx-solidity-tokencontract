535dff318b822e3f8f233e46798d2251753a0bbf
flisko <ziga.flis@student.um.si>
2020-02-03 20:01:50 +0100

    updated version

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
           host: 'localhost',
           port: 8545,
           network_id: '3',
-          gas: 4000000,
+          gas: 10000000,
           gasPrice: 180e9,
           // following address needed to be replaced with unlocked account on gwan node
           from: '0x14ef9a5100dc4dccda028fa4936e63e31c75c5e1'
@@ -12,7 +12,14 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version:"0.5.11"  // ex:  "0.4.20". (Default: Truffle's installed solc)
-    }
+      version:"0.5.11",
+      evmVersion:"byzantium",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200   // Optimize for how many times you intend to run the code
+        }   // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }   
+}
 }
 }
